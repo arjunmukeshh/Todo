69035dc8593c1721b11734be306252077c327d2a
Arjun M <60224351+arjunmukeshh@users.noreply.github.com>
2023-02-16 22:49:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="App list-group-item  justify-content-center align-items-center mx-auto" style={{"width":"400px", "backgroundColor":"white", "marginTop":"15px"}} >
       <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">Task Manager</h1>
-      <h6 className="card text-white bg-primary mb-3">FASTAPI - React - MongoDB</h6>
+   
      <div className="card-body">
       <h5 className="card text-white bg-dark mb-3">Add Your Task</h5>
       <span className="card-text"> 
